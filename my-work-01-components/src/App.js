465127184eeca6165401255aae1fd98c3eaf0a79
Vovanchuk04071 <vovanchuk04071@gmail.com>
2021-01-23 07:18:00 +0200

    test8

--- my-work-01-components/src/App.js
+++ my-work-01-components/src/App.js
@@ -1,12 +1,12 @@
 // import React from 'react';
-// import Profile from './Component/Profile';
+import Profile from './Component/Profile';
 import user from './user.json';
 console.log(user);
 
 const App = () => (
     <ul>
         <li>
-            <Profile
+            <
                 name={user.name}
                 tag={user.tag}
                 location={user.location}
